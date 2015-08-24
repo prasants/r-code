2d6b4452b4abb7a1e7e2448122db24cb37587885
Prasant Sudhakaran <prasant@prasant.net>
2015-08-24 23:38:20 +0530

    Updated till Chapter 5

--- rforeveryone.R
+++ rforeveryone.R
@@ -135,3 +135,161 @@ factor(x=c("High School", "College", "Masters", "Doctorate"), levels=c("High Sch
 #Calling Functions####
 #For example, calling the 'mean' function
 mean(x)
+#Missing Data####
+#Two types of missing data in R - NA and NULL
+
+#To test for missing values, use is.na
+z <- c(1,2,NA, 8, 3, NA, 3)
+z
+is.na(z)
+zChar <- c("Hockey", NA, "Lacrosse")
+zChar
+is.na(zChar)
+
+#NULL - absense of anything
+#Null cannot be used inside a vector
+#Test for null using is.null
+
+d <- NULL
+is.null(d)
+
+#Data Frames####
+#Each column in a Data Frame is a vector, each of which has the same length
+
+#Constructing a Data Frame
+x <- 10:1
+y <- -4:5
+q <- c("Hockey", "Football", "Baseball", "Curling", "Rugby", "Lacrosse", "Baksetball", "Tennis", "Cricket", "Soccer")
+theDF <- data.frame(x,y,q)
+theDF
+#The above code created a 10X3 data frame
+
+#Assigning names to the columns
+theDF <- data.frame(First = x, Second = y, Sport = q )
+theDF
+
+nrow(theDF)
+ncol(theDF)
+dim(theDF)
+
+#Checking the column names
+names(theDF)
+#Checking specific column name
+names(theDF)[3]
+
+#Check row names of a data frame
+rownames(theDF)
+#Assign row names of a data frame
+rownames(theDF) <- c("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten")
+rownames(theDF)
+#Set names back to generic index
+rownames(theDF) <- NULL
+rownames(theDF)
+
+head(theDF)
+head(theDF, n=7)
+tail(theDF)
+tail(theDF, n=7)
+class(theDF)
+
+#Accessing individual columns of a dataframe
+theDF$Sport
+#Individual elements can also be accessed 
+theDF[3,2] #Accessing the item in the 3rd row, 2nd column
+#Soecifying more than one row or column by using vector indices
+theDF[3, 2:3] #Third row, columns 2 and 3
+theDF[2, 1:3] #Second row, all three columns
+
+theDF[c(3,5),2] #Row 3 and 5, column 2
+theDF[c(3,5), 2:3] #Row 3 and 5, column 2 through 3
+
+theDF[1,] #Accessing Row, but not mentioning any column. So it selects the entire row
+theDF[,3] #Accessing entire column
+theDF[,2:3] #Accessing columns 2 through 3
+theDF[2:4,] #Accessing rows 2 through 4
+theDF[c("First", "Sport")] #Accessing multiple columns by name
+theDF[,"Sport"] #Accessing a column by name
+
+class(theDF[,"Sport"])
+theDF["Sport"]
+class(theDF["Sport"])
+class(theDF[["Sport"]])
+
+#Model Matrix
+newFactor <- factor(c("Pennsylvania", "New York", "New Jersey", "New York", "Tennessee", "Massachusetts", "Pennsylvania", "New York"))
+model.matrix(~newFactor -1)
+
+#Lists####
+#A Container to hold arbitrary objects of either the same type of varying types
+list(1,2,3)
+list(c(1,2,3))
+(list3 <- list(c(1,2,3), 3:7)) #Creates a two element list. First is a 3 vector element, second is a 5 vector element.
+
+list(theDF, 1:10)
+list5 <- list(theDF, 1:10, list3)
+list5
+names(list5)
+names(list5) <- c("data.frame", "vector", "list")
+names(list5)
+list5
+
+list6 = list(TheDataFrame = theDF, TheVector = 1:10, TheList = list3)
+names(list6)
+list6
+(emptylist <- vector(mode="list", length=4))
+list5[[1]]
+list5[[1:3]]
+
+list5[["data.frame"]]
+#Nested indexing of documents
+list5[[1]]$Sport
+list5[[1]][,"Second"]
+length(list5)
+list5
+#Add a 4th element to list5
+list5[[4]] <- 2
+list5
+names(list5)
+
+#Matrices####
+#Similar to data.frame, but every single element in a Matrix must be of the same type.
+
+#Create a 5X2 matrix
+A <- matrix(1:10, nrow=5)
+#Create another 5X2 matrix
+B <- matrix(21:30, nrow=5)
+#Create one more 5X2 matrix
+C <- matrix(21:40, nrow=2)
+A
+B
+C
+nrow(A)
+nrow(B)
+nrow(C)
+dim(A)
+#Add A and B
+A+B
+#Multiply them
+A*B
+#Check if elements are equal
+A==B
+#Trasposing B
+A %*% t(B)
+colnames(A)
+rownames(A)
+colnames(A) <- c("Left", "Right")
+rownames(A) <- c("1st", "2nd", "3rd", "4th", "5th")
+colnames(A)
+rownames(A)
+#Transposing flips the row and column names
+t(A)
+colnames(C) <- LETTERS[1:10] #'letters' vs 'LETTERS' 
+rownames(C) <- c("Top", "Bottom")
+A%*%C
+
+#Arrays####
+# An array is a multi-dimensional vector
+theArray <- array(1:12, dim=c(2,3,2)) #Dimensions - 2 rows, 3 columns, 2 "tables"
+theArray
+
+theArray[1,,]
