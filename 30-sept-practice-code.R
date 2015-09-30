3f61efa8c8e191108006edbd1f2ab01e8a12283b
prasants <prasant@prasant.net>
2015-09-30 14:15:48 +0530

    Variable and Datatypes

---
+++ 30-sept-practice-code.R
@@ -0,0 +1,24 @@
+#More R Practice Code####
+
+#Variables####
+x <- 2
+x
+
+y=5
+y
+#Multiple variable assignment
+a <- b <- 7
+a
+b
+
+#Alternate way to assign value - although more time consuming
+assign("j", 4)
+j
+
+#Removing Variables
+j
+rm(j)
+j #Will now return the error message "object 'j' not found"
+ #Data Types####
+class(x)
+x
